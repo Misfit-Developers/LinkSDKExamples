5ea098516bb5ffe73627db16c93c3168be242937
Phill Pasqual <phill@misfit.com>
2015-12-02 15:44:56 +0800

    LapTimer init commit

---
+++ LapTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LapTimer
+//
+//  Created by Phillip Pasqual on 12/2/15.
+//  Copyright © 2015 misfitwearables. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
